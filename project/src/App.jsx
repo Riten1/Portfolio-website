3d41ffa42179cd8b34c448cb166823fb4fe17026
Riten1 <122423532+Riten1@users.noreply.github.com>
2024-02-14 12:19:34 +0545

    'Router'

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -1,10 +1,22 @@
+import { Outlet, useNavigate } from 'react-router-dom'
 import './App.css'
+import Header from './components/Header/Header'
+import { useEffect } from 'react'
 
 function App() {
+const navigate = useNavigate()
+
+  useEffect(() => {
+    navigate('/')
+  })
 
   return (
     <>
-     
+     <Header />
+     <main>
+      <Outlet />
+     </main>
+     <Footer /> 
     </>
   )
 }
--- project/src/pages/About.jsx
+++ project/src/pages/About.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const Home = () => {
+const About = () => {
   return (
-    <div>Home</div>
+    <div>About</div>
   )
 }
 
-export default Home
+export default About
