c8f2785ed9c4489c7211d1aaa911dcb236ba3b37
Riten1 <122423532+Riten1@users.noreply.github.com>
2024-02-17 14:17:41 +0545

    'navBarComplete'

--- project/src/components/Header/Header.jsx
+++ project/src/components/Header/Header.jsx
@@ -1,59 +1,91 @@
+// import { useState } from "react";
 import React from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+
 
 const Header = () => {
-  const navigate = useNavigate();
+ 
+
+  const {pathname} = useLocation()
+ 
+  let location = pathname.split('/')?.[1]
+   
+
+
+  function link(type = null) {
+     
+    if(location === ''){
+      location = 'about'
+    }
+    let classes = 'no-underline flex text-white  pl-10 pr-10 pt-5 pb-5 font-mont cursor-pointer border-y-0 border-x-2 border-l-0 border-white border-solid hover:bg-indigo-400 duration-200'
+    if(location === type){
+      classes += ' bg-indigo-400'
+    }else{
+      classes +=  ' bg-indigo-950'
+    }
+    return classes
+  }
+
   const navItems = [
     {
       name: "About",
       path: "/",
+      link: 'about'
     },
     {
       name: "Skills",
       path: "/skills",
+      link: 'skills'
     },
     {
       name: "Services",
       path: "/services",
+      link: 'services'
     },
     {
       name: "Experience",
       path: "/experience",
+      link: 'experience'
     },
     {
       name: "Education",
       path: "/education",
+      link: 'education'
     },
     {
       name: "Portfolio",
       path: "/portfolio",
+      link: 'portfolio'
     },
     {
       name: "Testimonials",
       path: "/testimonials",
+      link:'testimonials'
     },
     {
       name: "Contact",
       path: "/contact",
+      link: 'contact'
     },
   ];
+
   return (
     <div className="">
       
       <nav className="">
       
-        <div className="flex bg-indigo-950 text-white justify-center items-center">
-        <Link to={'/'}>
-          <div className=" font-mont">Riten<span>.</span>CV</div>
+        <div className="flex justify-start items-center pt-2">
+        <Link to={'/'} className="no-underline">
+          <div className='font-mont text-3xl text-indigo-950 font-extrabold no-underline pl-6' >Riten<span>.</span>CV</div>
         </Link>
         <ul className="flex">
           {navItems.map((item) => (
-            <div className="flex font-mont border-black items-center" key={item.name} onClick={() => navigate(item.path)}>
+            <Link to={item.path} className={link(item.link)} key={item.name}>
                {item.name}
-            </div>)
+            </Link>)
              )}
           </ul>
-       
+          
         </div>
          
       </nav>
