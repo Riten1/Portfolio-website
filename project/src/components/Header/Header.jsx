67bca810fe50093f19f12870038237ee54630162
Riten1 <122423532+Riten1@users.noreply.github.com>
2024-02-17 15:18:24 +0545

    'NavbarFormatted'

--- project/src/components/Header/Header.jsx
+++ project/src/components/Header/Header.jsx
@@ -2,92 +2,89 @@
 import React from "react";
 import { Link, useLocation } from "react-router-dom";
 
-
 const Header = () => {
- 
+  const { pathname } = useLocation();
 
-  const {pathname} = useLocation()
- 
-  let location = pathname.split('/')?.[1]
-   
-
+  let location = pathname.split("/")?.[1];
 
   function link(type = null) {
-     
-    if(location === ''){
-      location = 'about'
+    if (location === "") {
+      location = "about";
     }
-    let classes = 'no-underline flex text-white  pl-6 pr-6 pt-5 pb-5 font-mont cursor-pointer border-y-0 border-x-2 border-l-0 border-white border-solid hover:bg-indigo-400 duration-200'
-    if(location === type){
-      classes += ' bg-indigo-400'
-    }else{
-      classes +=  ' bg-indigo-950'
+    let classes =
+      "no-underline flex text-white  pl-6 pr-6 pt-5 pb-5 font-mont cursor-pointer border-y-0 border-x-2 border-l-0 border-white border-solid hover:bg-indigo-400 duration-200";
+    if (location === type) {
+      classes += " bg-indigo-400";
+    } else {
+      classes += " bg-indigo-950";
     }
-    return classes
+    return classes;
   }
 
   const navItems = [
     {
       name: "About",
       path: "/",
-      link: 'about'
+      link: "about",
     },
     {
       name: "Skills",
       path: "/skills",
-      link: 'skills'
+      link: "skills",
     },
     {
       name: "Services",
       path: "/services",
-      link: 'services'
+      link: "services",
     },
     {
       name: "Experience",
       path: "/experience",
-      link: 'experience'
+      link: "experience",
     },
     {
       name: "Education",
       path: "/education",
-      link: 'education'
+      link: "education",
     },
     {
       name: "Portfolio",
       path: "/portfolio",
-      link: 'portfolio'
+      link: "portfolio",
     },
     {
       name: "Testimonials",
       path: "/testimonials",
-      link:'testimonials'
+      link: "testimonials",
     },
     {
       name: "Contact",
       path: "/contact",
-      link: 'contact'
+      link: "contact",
     },
   ];
 
   return (
     <div className="">
-      
       <nav className="">
-      
         <div className="flex justify-start items-center pt-9">
-        <Link to={'/'} className="no-underline">
-          <div className='font-mont text-4xl text-indigo-950 font-extrabold no-underline pl-10 mr-12' >Riten<span className="font-mont text-3xl font-extrabold text-indigo-400">.</span>CV</div>
-        </Link>
-        <ul className="flex">
-          {navItems.map((item) => (
-            <Link to={item.path} className={link(item.link)} key={item.name}>
-               {item.name}
-            </Link>)
-             )}
+          <Link to={"/"} className="no-underline">
+            <div className="font-mont text-4xl text-indigo-950 font-extrabold no-underline pl-10 mr-12">
+              Riten
+              <span className="font-mont text-3xl font-extrabold text-indigo-400">
+                .
+              </span>
+              CV
+            </div>
+          </Link>
+          <ul className="flex">
+            {navItems.map((item) => (
+              <Link to={item.path} className={link(item.link)} key={item.name}>
+                {item.name}
+              </Link>
+            ))}
           </ul>
-          
         </div>
-         
       </nav>
     </div>
   );
