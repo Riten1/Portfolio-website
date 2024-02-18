fe42dbc754ff2a34dae187c4a37e850572535116
Riten1 <122423532+Riten1@users.noreply.github.com>
2024-02-18 20:53:21 +0545

    'images'

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -5,6 +5,7 @@ import Footer from "./components/Footer/Footer";
 import SuperContainer from "./components/container/SuperContainer";
 import MainContainer from "./components/container/MainContainer";
 import image from './components/images/riten.jpg'
+import icon1 from './components/images/icon_1.png.webp'
 
 function App() {
   const navigate = useNavigate();
@@ -18,7 +19,7 @@ function App() {
       <Header />
       <main>
         <MainContainer>
-          <div className="w-96 h-full">
+          <div className="w-96 h-full bg-indigo-950">
             <div className="block relative">
               <div className="w-full h-96">
               <div className="absolute top-0 left-0 w-full h-full bg-no-repeat bg-cover bg-center">
@@ -26,6 +27,18 @@ function App() {
           </div>
               </div>
            
+            </div>
+            <div className="w-full h-full pl-4 pt-12 pr-4 pb-20 overflow-hidden">
+              <div className="text-3xl font-semibold text-white leading-tight text-center">
+                General Information
+              </div>
+              <ul className="mt-8">
+                <li className="flex items-start justify-start">
+                  <div className="w-5 h-5 mr-6">
+                    <img src={icon1}></img>
+                  </div>
+                </li>
+              </ul>
             </div>
         
           </div>
