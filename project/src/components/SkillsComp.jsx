a861b363295030979eb6117404efa8ba5eb8ff8c
Riten1 <122423532+Riten1@users.noreply.github.com>
2024-02-20 19:46:40 +0545

    'skill'

---
+++ project/src/components/SkillsComp.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const SkillsComp = () => {
+  return (
+    <div className=" items-start justify-end pl-20 pt-20 pr-10 w-3/4">
+      </div>
+  )
+}
+
+export default SkillsComp
