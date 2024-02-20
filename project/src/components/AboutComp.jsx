c1e655b9bc9f0b00a5a6e0bb5c779a2d1797df85
Riten1 <122423532+Riten1@users.noreply.github.com>
2024-02-20 18:12:39 +0545

    'AboutFormatted

--- project/src/components/AboutComp.jsx
+++ project/src/components/AboutComp.jsx
@@ -1,17 +1,16 @@
-import React from 'react'
+import React from "react";
 
 const AboutComp = () => {
   return (
-    <div className=' items-start justify-end ml-20 mt-20'>
-     
-      <div className='text-3xl font-medium text-gray-500'>
-   
-   Frontend Developer</div>
-   <div className='text-7xl font-extrabold text-indigo-950 mt-3 '>Riten Gurung</div>
+    <div className=" items-start justify-end ml-20 mt-20">
+      <div className="text-3xl font-medium text-gray-500">
+        Frontend Developer
       </div>
-  
-    
-  )
-}
+      <div className="text-7xl font-extrabold text-indigo-950 mt-3 ">
+        Riten Gurung
+      </div>
+    </div>
+  );
+};
 
-export default AboutComp
+export default AboutComp;
